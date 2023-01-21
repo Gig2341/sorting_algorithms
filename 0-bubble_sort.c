f4e231f02f7f5c1f010acc81a332de8baba81b98
Gig2341 <danmata2341@gmail.com>
2023-01-21 21:15:41 +0000

    task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,50 @@
+#include "sort.h"
+
+/**
+ * swap_ints - a function  that swaps integers
+ * @ a - fisrt integer
+ * @ b - second integer
+ * Returns: Null
+ */
+
+
+void swap_ints(int *a, int *b)
+{
+	int temp;
+	
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+
+/**
+ * bubble_sort - function that bubble sorts integers
+ * @ array - an array to be sorted
+ * @ size - the size of the array
+ * Returns: a bubble sorted array in ascending order
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t len = size;
+	size_t i = 0;
+	bool swapped = false;
+
+	if (array == NULL || size < 2)
+		return;
+
+	while( swapped == false)
+	{
+		for (i =0; i < len-1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				swap_ints(array + i,(array + i) + 1);
+				print_array(array, size);
+			}
+		}
+		len--;
+	}	
+
+}
