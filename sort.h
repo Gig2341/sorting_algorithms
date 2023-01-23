1fa156170d6de2aac01f70595688adf531fd5774
Gig2341 <danmata2341@gmail.com>
2023-01-23 19:08:19 +0000

    task 3

--- sort.h
+++ sort.h
@@ -18,4 +18,7 @@ typedef enum bool
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void selection_sort(int *array, size_t size);
+int lomuto_partition(int *array, size_t size, int left, int right);
+void lomuto_sort(int *array, size_t size, int left, int right);
+void quick_sort(int *array, size_t size);
 #endif
