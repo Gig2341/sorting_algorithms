0954d010adde24d2f8dec565ede0f0924e0f69f0
Segun Nuhu Olawale <96541838+Olawalesegun@users.noreply.github.com>
2023-01-23 02:35:22 -0800

    Update printlist

--- print_list.c
+++ print_list.c
@@ -21,7 +21,6 @@ void print_list(const listint_t *list)
         list = list->next;
     }
     printf("\n");
-=======
 	int i;
 
 	i = 0;
@@ -34,5 +33,4 @@ void print_list(const listint_t *list)
 		list = list->next;
 	}
 	printf("\n");
->>>>>>> 696f3740400b3190e720e1c9894059b4a3bcf3c1
 }
