b0b05d6e5c1bdbf751951fabd41a83822695146f
Olawalesegun <olawalesegun0@gmail.com>
2023-01-23 12:17:50 +0000

    Insertion task and compiled files

--- print_list.c
+++ print_list.c
@@ -8,7 +8,6 @@
  */
 void print_list(const listint_t *list)
 {
-<<<<<<< HEAD
     int i;
 
     i = 0;
@@ -21,18 +20,4 @@ void print_list(const listint_t *list)
         list = list->next;
     }
     printf("\n");
-=======
-	int i;
-
-	i = 0;
-	while (list)
-	{
-		if (i > 0)
-			printf(", ");
-		printf("%d", list->n);
-		++i;
-		list = list->next;
-	}
-	printf("\n");
->>>>>>> 696f3740400b3190e720e1c9894059b4a3bcf3c1
 }
